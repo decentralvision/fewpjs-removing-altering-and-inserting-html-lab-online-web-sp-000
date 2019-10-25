5ab8f0ee394260e046d671af0918c45e8157dd9d
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-10-25 08:20:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // Write your code here!
-$("main").remove
-let newHeader = document.createElement('h1#victory')
+document.querySelector("main#main").remove();
+const newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
